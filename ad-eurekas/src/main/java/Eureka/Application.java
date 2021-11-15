4228ec9ceb078caf25547d4596db86e717b867fc
zhang <1819592017@qq.com>
2021-11-15 15:15:28 +0800

    successs

---
+++ ad-eurekas/src/main/java/Eureka/Application.java
@@ -0,0 +1,14 @@
+package Eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
